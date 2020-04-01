04c60ada8f13659979d14d0cb3d301faec0dbf4a
shahnshah <malik@estater.com>
2020-04-02 00:15:18 +0530

    first commit

---
+++ sw.js
@@ -0,0 +1,28 @@
+const version = "1.0.0";
+const cacheName = `pwaExample-${version}`;
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                `/`,
+                `/index.html`,
+                `/main.css`,
+            ])
+                .then(() => self.skipWaiting());
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open(cacheName)
+            .then(cache => cache.match(event.request, { ignoreSearch: true }))
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
